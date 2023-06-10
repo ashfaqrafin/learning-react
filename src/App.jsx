62b18b4e865658d1b1b08877b2b061ae5b4b12ec
Ashfaq Khan Rafin <yourgitemail@example.com>
2023-06-10 13:30:24 +0600

    completed-action

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+import Home from "./Home";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
+};
+
+export default App;
